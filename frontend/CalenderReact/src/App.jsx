86010834f57b5e0471f99dcd1e8a42d00ce225a6
Ashwath Kallam <ashwathkallam@gmail.com>
2025-04-20 11:30:25 -0700

    Some javaScript work

--- frontend/CalenderReact/src/App.jsx
+++ frontend/CalenderReact/src/App.jsx
@@ -4,8 +4,48 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [preReg, setPreReg] = useState('No')
+  const [Duration, setDuration] = useState(0)
+  const [name, setName] = useState('')
+  const [start, setStart] = useState('')
+  const [end, setEnd] = useState('')
+  const [priority,setPriority] = useState('')
+  const [weekDay, setWeekDay] = useState('')
 
-}
+  const checkIfRegistered = () => {
+    let EventData = {Name: name}
+  
+    if (preReg.toLowerCase() === 'Yes') {
+      EventData.Start = start
+      EventData.WeekDay = weekDay
+      EventData.End = end
+    }
+    else {
+      EventData.Duration = Duration
+      EventData.Priority = Priority
+    }
+  }
+
+  return (
+    <main>
+    <h1> Schedule Optimizer</h1>
+      <input
+        type = 'submit'
+        placeholder='Is this a pre-scheduled event? (Yes/No)'
+        value = {preReg}
+        setValue = {(e) => setPreReg(e.target.value)}
+        ></input>
+
+        <button>
+          type = 'submit'
+          onclick = {checkIfRegistered}
+        </button>
+        </main>
+        
+  )
+
+
+
+ }
 
 export default App
