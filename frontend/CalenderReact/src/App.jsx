c211f80101f8465ddce6a5e7869805d3d301e87b
Ashwath Kallam <ashwathkallam@gmail.com>
2025-04-22 19:52:30 -0700

    updated react for added API

--- frontend/CalenderReact/src/App.jsx
+++ frontend/CalenderReact/src/App.jsx
@@ -4,95 +4,59 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [preReg, setPreReg] = useState('No')
-  const [duration, setDuration] = useState(0)
+  const [duration, setDuration] = useState('')
   const [name, setName] = useState('')
-  const [start, setStart] = useState('')
-  const [end, setEnd] = useState('')
   const [priority,setPriority] = useState('')
-  const [weekDay, setWeekDay] = useState('')
-
-  const [preRegFields, setPreRegFields] = useState(false)
-  const[eventFields,setEventFields] = useState(false)
-
-  const handlePreRegChange = () => {
-    if (preReg.toLocaleLowerCase() === 'yes'){
-      setPreRegFields(true)
-      setEventFields(false)
-    } else {
-      setPreRegFields(false)
-      setEventFields(true)
-    }
-  }
 
   return (
     <main>
+      <div id = 'user_Inputs'>
       <h1> Schedule Optimizer</h1>
+      <div>
       <input
         type="text"
-        placeholder="Is this a pre-scheduled event? (Yes/No)"
-        value={preReg}
-        onChange={(e) => setPreReg(e.target.value)}
+        placeholder="Name of the event"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
+      </div>
 
+      <div>
+      <input 
+        type = 'number'
+        placeholder = 'Expected Duration'
+        value = {duration}
+        onChange = {(e) => setDuration(e.target.value)} 
+        />
+        </div>
+       
+       <div>
+      <input
+        type = 'number'
+        placeholder = 'Event Priority (1-5)'
+        value = {priority}
+        onChange = {(e) => setPriority(e.target.value)}
+        />
+        </div>
+        </div>
+      <div>
         <button
           type = 'submit'
-          onclick = {handlePreRegChange}
-        > Add </button>
+          > Submit your responses
+          </button>
+      </div>
 
-        {preRegFields && (
-          <div>
-            <h2> PreScheduled Event Details</h2>
-            <input
-            type = 'submit'
-            placeholder = 'Duration of Event'
-            value = {duration}
-            onChange = {(e) => setDuration(e.target.value)}>
-            </input>
-           
-            <input
-            type = 'number'
-            placeholder = 'Enter Priority (Highest 5, lowest 1)'
-            value = {priority}
-            onChange = {(e) => setPriority(e.target.value)}>
-            </input>
-          </div>
-        )}
-
-        {eventFields && (
-          <div>
-            <input
-            type = 'time'
-            placeholder = 'start Time'
-            value = {start}
-            onChange = {(e) => setStart(e.target.value)}>
-            </input>
-
-            <input
-            type = 'time'
-            placeholder = 'End time'
-            value = {end}
-            onChange = {(e) => setEnd(e.target.value)}>
-            </input>
-
-            <input
-            type = 'text'
-            placeholder = 'Weekday'
-            value = {weekDay}
-            onChange = {(e) => setWeekDay(e.target.value)}>
-            </input>
-
-          </div>
-
-
-        )}
-        </main>
+      </main>
+      )
+      }
+        
+        
      
 
-  )
+  
 
 
 
- }
+ 
 
 export default App
