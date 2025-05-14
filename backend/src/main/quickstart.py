93ddf2fcca4854ac602e8df8a5527c883997e7cd
Ashwath Kallam <ashwathkallam@gmail.com>
2025-05-14 02:18:25 -0700

    port

--- backend/src/main/quickstart.py
+++ backend/src/main/quickstart.py
@@ -31,7 +31,7 @@ def load_Events_Into(sched):
       flow = InstalledAppFlow.from_client_secrets_file(
           "credentials.json", SCOPES
       )
-      creds = flow.run_local_server(port=0)
+      creds = flow.run_local_server(port=8080)
     # Save the credentials for the next run
     with open("token.json", "w") as token:
       token.write(creds.to_json())
